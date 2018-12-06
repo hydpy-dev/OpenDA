b5dcd8052201338f6fd3a388f8da4745f5a49567
Christoph Tyralla <c.tyralla@bjoernsen.de>
2018-12-06 08:27:17 +0100

    Add `run_doctests.py`.

---
+++ run_doctests.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Collect all `README.md` files and execute their doctests."""
+import doctest
+import os
+import sys
+import numpy
+
+homedir = os.path.abspath('.')
+counter = numpy.zeros(2, dtype=int)
+
+for root, _, filenames in os.walk('.'):
+    filenames = [fn for fn in filenames if fn == 'README.md']
+    for filename in filenames:
+        path = os.path.join(root, filename)
+        print(path)
+        os.chdir(root)
+        counter += numpy.array(doctest.testfile(filename))
+        os.chdir(homedir)
+
+at_least_one_test_failed = bool(counter[0])
+sys.exit(at_least_one_test_failed)
+

df09bd5d0f44acd51a8047698827303b3e65d7c4
Christoph Tyralla <c.tyralla@bjoernsen.de>
2018-12-07 22:02:59 +0100

    Control the HydPy server in example `SeqSim` with more atomic HTTP requests ('run_HydPy.bat`).

--- demos/openda_projects/SeqSim/run_HydPy.bat
+++ demos/openda_projects/SeqSim/run_HydPy.bat
@@ -1,1 +1,7 @@
-curl -v --data-binary "@C:\HydPy\OpenDA\demos\openda_projects\SeqSim\results/temp0/hydpy.exchange" http://localhost:8080/process_input?id=test -o C:\HydPy\OpenDA\demos\openda_projects\SeqSim\results/temp0/hydpy.exchange
+curl --data-binary "@C:\HydPy\OpenDA\demos\openda_projects\SeqSim\results/temp0/hydpy.exchange" http://localhost:8080/period?id=test
+curl --data-binary "@C:\HydPy\OpenDA\demos\openda_projects\SeqSim\results/temp0/hydpy.exchange" http://localhost:8080/parametervalues?id=test
+curl http://localhost:8080/load_conditions?id=test
+curl --data-binary "@C:\HydPy\OpenDA\demos\openda_projects\SeqSim\results/temp0/hydpy.exchange" http://localhost:8080/conditionvalues?id=test
+curl http://localhost:8080/simulate?id=test
+curl http://localhost:8080/save_conditionvalues?id=test
+curl http://localhost:8080/itemvalues?id=test -o C:\HydPy\OpenDA\demos\openda_projects\SeqSim\results/temp0/hydpy.exchange

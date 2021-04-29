27293f9a4c67b2155d12f10ba46c582fc642a5ba
Belger <belger@bce01.de>
2021-04-29 14:02:10 +0200

    Implementing IDataObject instead of old IoObjectInterface

--- extensions/HydPyOpenDABBModelWrapper/src/main/java/org/hydpy/openda/HyPyIoObject.java
+++ extensions/HydPyOpenDABBModelWrapper/src/main/java/org/hydpy/openda/HyPyIoObject.java
@@ -12,36 +12,41 @@
 package org.hydpy.openda;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.hydpy.openda.server.HydPyModelInstance;
 import org.hydpy.openda.server.HydPyServerException;
 import org.hydpy.openda.server.HydPyServerManager;
-import org.openda.blackbox.interfaces.IoObjectInterface;
+import org.openda.interfaces.IDataObject;
 import org.openda.interfaces.IExchangeItem;
+import org.openda.interfaces.IExchangeItem.Role;
 
 /**
  * @author Gernot Belger
  */
-public final class HyPyIoObject implements IoObjectInterface
+public final class HyPyIoObject implements IDataObject
 {
-  private List<IExchangeItem> m_exchangeItems = null;
+  private final Map<String, IExchangeItem> m_exchangeItems = new HashMap<>();
 
   private String m_instanceId = null;
 
   @Override
-  public void initialize( final File workingDir, final String fileName, final String[] arguments )
+  public void initialize( final File workingDir, final String[] arguments )
   {
     try
     {
-      if( arguments.length != 1 )
-        throw new RuntimeException( "IO-Object expects exactly one argument, the instance-number" );
+      if( arguments.length != 2 )
+        throw new RuntimeException( "IO-Object expects exactly two arguments, the second one is the instance-number" );
 
-      m_instanceId = arguments[0];
+      m_instanceId = arguments[1];
 
       final HydPyModelInstance server = HydPyServerManager.instance().getOrCreateInstance( m_instanceId );
 
-      m_exchangeItems = server.getItemValues();
+      final List<IExchangeItem> exchangeItems = server.getItemValues();
+      for( final IExchangeItem item : exchangeItems )
+        m_exchangeItems.put( item.getId(), item );
     }
     catch( final HydPyServerException e )
     {
@@ -52,15 +57,31 @@ public final class HyPyIoObject implements IoObjectInterface
   }
 
   @Override
-  public IExchangeItem[] getExchangeItems( )
+  public String[] getExchangeItemIDs( )
   {
-    return m_exchangeItems.toArray( new IExchangeItem[m_exchangeItems.size()] );
+    return m_exchangeItems.keySet().toArray( new String[m_exchangeItems.size()] );
+  }
+
+  @Override
+  public String[] getExchangeItemIDs( final Role role )
+  {
+    return m_exchangeItems.values()//
+        .stream()//
+        .filter( item -> item.getRole() == role )//
+        .map( item -> item.getId() )//
+        .toArray( String[]::new );
+  }
+
+  @Override
+  public IExchangeItem getDataObjectExchangeItem( final String exchangeItemID )
+  {
+    return m_exchangeItems.get( exchangeItemID );
   }
 
   @Override
   public void finish( )
   {
     final HydPyModelInstance server = HydPyServerManager.instance().getOrCreateInstance( m_instanceId );
-    server.setItemValues( m_exchangeItems );
+    server.setItemValues( m_exchangeItems.values() );
   }
 }
